c98f63d680600450c52021fe70ea25206f940d69
ezhov-da <ezhdenis@yandex.ru>
2023-05-03 21:39:55 +0300

    init

--- application/src/main/java/ru/ezhov/umnei/mathematics/imagegenerator/ImageGeneratorApplicationService.java
+++ application/src/main/java/ru/ezhov/umnei/mathematics/imagegenerator/ImageGeneratorApplicationService.java
@@ -1,9 +1,9 @@
 package ru.ezhov.umnei.mathematics.imagegenerator;
 
 import org.springframework.stereotype.Service;
-import ru.ezhov.imagegenerator.api.ImageGenerator;
-import ru.ezhov.imagegenerator.api.ImageGeneratorException;
-import ru.ezhov.imagegenerator.api.ImageGeneratorFactory;
+//import ru.ezhov.imagegenerator.api.ImageGenerator;
+//import ru.ezhov.imagegenerator.api.ImageGeneratorException;
+//import ru.ezhov.imagegenerator.api.ImageGeneratorFactory;
 import ru.ezhov.mathematics.engine.task.domain.model.HiddenValueNumber;
 import ru.ezhov.mathematics.engine.task.domain.model.Task;
 import ru.ezhov.mathematics.engine.task.domain.model.Tasks;
@@ -27,33 +27,36 @@ public class ImageGeneratorApplicationService {
                               String actions,
                               int from,
                               int to) throws ImageGeneratorApplicationServiceException {
-        try {
-            Tasks tasks = mathematicsApplication.generate(type, actions, from, to);
-            List<Task> all = tasks.all();
-            List<Task> randomTask = new ArrayList<>();
-            for (int i = 0; i < all.size(); i++) {
-                randomTask.add(all.get(new Random().nextInt(all.size())));
-            }
 
-            List<String> values = randomTask.stream().map(t -> {
-                final HiddenValueNumber hiddenValueNumber = t.hiddenValueNumber();
-                return String.format("%s %s %s = %s",
-                        hiddenValueNumber == HiddenValueNumber.FIRST ? "__" : t.firstValue().value(),
-                        t.mathOperator().symbol(),
-                        hiddenValueNumber == HiddenValueNumber.SECOND ? "__" : t.secondValue().value(),
-                        hiddenValueNumber == HiddenValueNumber.FIRST ? t.firstValue().value() :
-                                hiddenValueNumber == HiddenValueNumber.SECOND ? t.secondValue().value() : t.thirdValue().value()
-                );
-            }).collect(Collectors.toList());
+        return new byte[0];
 
-            final ImageGenerator imageGenerator = ImageGeneratorFactory.getInstance("Примеры по математике", "умней.рус", values);
-
-            return imageGenerator.generate();
-        } catch (MathematicsApplicationException e) {
-            throw new ImageGeneratorApplicationServiceException("Ошибка генерации примеров", e);
-        } catch (ImageGeneratorException e) {
-            throw new ImageGeneratorApplicationServiceException("Ошибка создания изображения", e);
-        }
+//        try {
+//            Tasks tasks = mathematicsApplication.generate(type, actions, from, to);
+//            List<Task> all = tasks.all();
+//            List<Task> randomTask = new ArrayList<>();
+//            for (int i = 0; i < all.size(); i++) {
+//                randomTask.add(all.get(new Random().nextInt(all.size())));
+//            }
+//
+//            List<String> values = randomTask.stream().map(t -> {
+//                final HiddenValueNumber hiddenValueNumber = t.hiddenValueNumber();
+//                return String.format("%s %s %s = %s",
+//                        hiddenValueNumber == HiddenValueNumber.FIRST ? "__" : t.firstValue().value(),
+//                        t.mathOperator().symbol(),
+//                        hiddenValueNumber == HiddenValueNumber.SECOND ? "__" : t.secondValue().value(),
+//                        hiddenValueNumber == HiddenValueNumber.FIRST ? t.firstValue().value() :
+//                                hiddenValueNumber == HiddenValueNumber.SECOND ? t.secondValue().value() : t.thirdValue().value()
+//                );
+//            }).collect(Collectors.toList());
+//
+//            final ImageGenerator imageGenerator = ImageGeneratorFactory.getInstance("Примеры по математике", "умней.рус", values);
+//
+//            return imageGenerator.generate();
+//        } catch (MathematicsApplicationException e) {
+//            throw new ImageGeneratorApplicationServiceException("Ошибка генерации примеров", e);
+//        } catch (ImageGeneratorException e) {
+//            throw new ImageGeneratorApplicationServiceException("Ошибка создания изображения", e);
+//        }
 
     }
 }
