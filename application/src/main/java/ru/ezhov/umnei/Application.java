7d5ee82a34a84cc11d9698c631c2f79c2810b213
ezhov-da <ezhdenis@yandex.ru>
2023-05-03 22:23:50 +0300

    init

--- application/src/main/java/ru/ezhov/umnei/Application.java
+++ application/src/main/java/ru/ezhov/umnei/Application.java
@@ -1,12 +1,28 @@
 package ru.ezhov.umnei;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.ezhov.mathematics.engine.example.domain.model.FirstValue;
+import ru.ezhov.mathematics.engine.example.domain.model.PlainExampleGroupRepository;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+    private final PlainExampleGroupRepository plainExampleGroupRepository;
+
+    public Application(PlainExampleGroupRepository plainExampleGroupRepository) {
+        this.plainExampleGroupRepository = plainExampleGroupRepository;
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Override
+    public void run(String... args) throws Exception {
+        plainExampleGroupRepository.by(FirstValue.create(10));
+    }
 }
+
+
